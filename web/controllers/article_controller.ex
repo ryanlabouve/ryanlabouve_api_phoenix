56cc8f667ca5ded43da9f36ea72729f61ad43aec
Ryan LaBouve <imryanlabouve@gmail.com>
2017-04-23 17:36:54 -0500

    Limit fields send with different queries

--- web/controllers/article_controller.ex
+++ web/controllers/article_controller.ex
@@ -6,29 +6,33 @@ defmodule RyanlabouveApiPhoenix.ArticleController do
 
   def index(conn, %{"filter" => %{"limit" => limit}}) do
     query = from a in Article,
-      limit: ^limit
+              select: map(a, [:id, :title, :date, :slug]),
+              limit: ^limit
     articles = Repo.all(query)
-    render(conn, "index.json", articles: articles)
+    render(conn, "index.json-api", articles: articles)
   end
 
   def index(conn, %{"filter" => %{"slug" => slug}}) do
     query = from a in Article,
       where: a.slug == ^slug
     articles = Repo.all(query)
-    render(conn, "index.json", articles: articles)
+    render(conn, "index.json-api", articles: articles)
   end
 
   def index(conn, %{"filter" => %{"term" => term}}) do
     query = from a in Article,
-      where: like(a.content, ^("%#{term}%"))
+              select: map(a, [:id, :title, :date, :slug]),
+              where: like(a.content, ^("%#{term}%"))
     articles = Repo.all(query)
-    render(conn, "index.json", articles: articles)
+    render(conn, "index.json-api", articles: articles)
   end
 
   def index(conn, _params) do
-    query = from article in Article,
+    query = from a in Article,
+              select: map(a, [:id, :title, :date, :slug, :description]),
               order_by: [desc: :date]
+
     articles = Repo.all(query)
-    render(conn, "index.json", articles: articles)
+    render(conn, "index.json-api", articles: articles)
   end
 end
